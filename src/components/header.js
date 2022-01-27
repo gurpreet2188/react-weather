c182e93ac5da7f2d8c65a47ce1f08521beebe0a2
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2022-01-27 22:03:00 +0800

    fixed header

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { GlobalData } from '../context/contexts';
 export function Header() {
     const { data } = useContext(GlobalData)
+    const name = JSON.parse(localStorage.getItem('name'))
     const cTemp = JSON.parse(localStorage.getItem('countries'))
     const location = JSON.parse(localStorage.getItem('location'))
     const [city, state, countryCode] = [location.city, location.state ? location.state : null, location.country]
@@ -11,10 +12,10 @@ export function Header() {
     useEffect(() => {
         cTemp.data.find(n => n.code === countryCode ? setCountry(n.name) : "")
     }, [cTemp])
-
+    console.log(city)
     return (
         <div className='home-header'>
-                <h1 className='header-title'>{city}</h1>
+                <h1 className='header-title'>{city ? city : name.name}</h1>
                 {/* <h1 className='header-subtitle'>{state ? state : ""}</h1>
                 <h1 className='header-subtitle'>{country}</h1> */}
         </div>
