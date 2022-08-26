2bdf82c3a7fbf1a2d59c46f5eb441bdfee8c56fe
Gurpreet Singh <gurpreet88.sandhu@gmail.com>
2022-08-26 22:24:20 +0800

    v4

--- src/App.js
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import './styles/font.css'
-import './styles/style.scss'
-import { SetData } from './context/setData';
-
-export default function App() {return <SetData/>}
--- src/context/contexts.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-export const GlobalColors = createContext(null)
-export const GlobalTime = createContext(null)
-export const GlobalData = createContext(null)
